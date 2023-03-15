270b38987b0b11528639b83c7d403a562e210840
Josh Weaver <jtw@digitalartbrokers.io>
2023-03-15 11:07:52 -0500

    Changed padding for the border style of model

--- styling/welcome-page-styling.go
+++ styling/welcome-page-styling.go
@@ -6,11 +6,13 @@ var HeaderStyle = lipgloss.NewStyle().
 	Foreground(lipgloss.Color("12")).
 	Background(lipgloss.Color("3"))
 
-var BorderStyle = lipgloss.NewStyle().
-	PaddingLeft(2).
-	PaddingTop(1)
-
 var PirateStyle = lipgloss.NewStyle()
 
 var HelpBarStyle = lipgloss.NewStyle().
 	PaddingLeft(2)
+
+var BorderStyle = lipgloss.NewStyle().
+	PaddingLeft(2).
+	PaddingTop(1).
+	PaddingRight(2).
+	PaddingBottom(1)
