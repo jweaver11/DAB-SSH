d7689774dd7a7551ab974a58ad41be66b26f0fb3
Josh Weaver <jtw@digitalartbrokers.io>
2023-03-16 13:19:46 -0500

    Added styling and padding to several aspects

--- styling/universal-styling.go
+++ styling/universal-styling.go
@@ -3,5 +3,4 @@ package styling
 import "github.com/charmbracelet/lipgloss"
 
 var NavBarStyle = lipgloss.NewStyle().
-	PaddingLeft(4).
-	PaddingRight(4)
+	PaddingLeft(1)
--- styling/welcome-page-styling.go
+++ styling/welcome-page-styling.go
@@ -4,16 +4,18 @@ import "github.com/charmbracelet/lipgloss"
 
 var HeaderStyle = lipgloss.NewStyle().
 	Foreground(lipgloss.Color("12")).
-	Background(lipgloss.Color("3"))
+	Background(lipgloss.Color("3")).
+	PaddingTop(1).
+	PaddingBottom(1)
 
 var PirateStyle = lipgloss.NewStyle().
-	Foreground(lipgloss.Color("#8a5715"))
+	Foreground(lipgloss.Color("55")).
+	PaddingTop(2).
+	PaddingBottom(2)
 
 var HelpBarStyle = lipgloss.NewStyle().
 	PaddingLeft(2)
 
 var BorderStyle = lipgloss.NewStyle().
 	PaddingLeft(2).
-	PaddingTop(1).
-	PaddingBottom(1).
 	Background(lipgloss.Color("#0c233b"))
