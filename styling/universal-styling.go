93089bb72e549e2957c76ea4f54bdbce08eb785b
Josh Weaver <jtw@digitalartbrokers.io>
2023-03-22 16:20:30 -0500

    Changed some styling

--- styling/universal-styling.go
+++ styling/universal-styling.go
@@ -6,7 +6,6 @@ var WaterMarkStyle = lipgloss.NewStyle().
 	Background(lipgloss.Color("25")) //#14C126
 
 var NavBarStyle = lipgloss.NewStyle().
-	PaddingLeft(1).
 	Faint(false)
 
 var HelpBarStyle = lipgloss.NewStyle().
