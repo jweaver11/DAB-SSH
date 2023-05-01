ec8e7420c88a7cba91ca5c25873d4d522b9d3630
Josh Weaver <jtw@digitalartbrokers.io>
2023-05-01 15:33:22 -0500

    Added content for buccaneers project

--- models/description.go
+++ models/description.go
@@ -7,7 +7,7 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-const useHighPerformanceRenderer = false
+//const useHighPerformanceRenderer = false
 
 type DescriptionPage struct {
 	projectName             string     // Watermark in top left corner of page
@@ -135,8 +135,30 @@ func (d DescriptionPage) View() string {
 	return styling.BorderStyle.Width(width).Height(height).Render(s)
 }
 
-var Descriptions = []string{
-	"deez",
+var Descriptions = []string{`Game Overview:
+BotB is a battle-card strategy game that combines choice-driven narrative features and programmed
+charitable impact. All in-game transactions are visible throughout the two-week experience,
+ensuring complete transparency for the players. The game's smart contracts guarantee a higher
+reward, unique to the NFT space: programmatically-assured charitable impact. Winning gamers will
+have the option to share their "spoils" with DAV for further impact.
+Charitable giving on the blockchain offers the promise of complete transparency. DAB has
+guaranteed that promise by disallowing human intervention in BotB's contract-cemented code. The
+game's strict parameters and automated controls circumvent "trust-based" agreements, which are
+vulnerable to human error, greed, or changeability. BotB self-monitors by way of a system built on
+total transparency.
+BotB's core objective is to do great while doing great. It challenges the existing NFT gaming
+community, the crypto-curious, and even the most skeptical crypto critics to get involved. Players
+will be empowered, entertained, and educated while supporting a truly deserving cause.
+	
+	Gameplay:
+In BotB, "Cards" or “player game pieces” are NFTs, and their value appreciates the more the game is
+played. Buccaneers in BotB have three categories of stats: physical, mental, and spiritual, and belong
+to one of three classes (human, cyborg, and robot), each starting out with the same stats for the 1st
+iteration. Battles are initiated by any player at any time. The game also features a defense mode that
+grants players an increased chance of winning if they are attacked.
+Interactive elements and battle outcomes are determined by the game's programming and informed
+by player choices. All actions carry a small cost, and the game continuously distributes these
+amounts to its main three pools: DAV, the treasure chest (prize), and DAB (BotB's team).`,
 	"coconuts",
 	"rule",
 	"yo"}
