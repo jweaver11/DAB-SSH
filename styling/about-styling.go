0d42f9fb21b1b9853926227b9f8ce37cb40dca20
Josh Weaver <jtw@digitalartbrokers.io>
2023-05-03 16:07:25 -0500

    Added some padding for the viewport

--- styling/about-styling.go
+++ styling/about-styling.go
@@ -4,5 +4,8 @@ package styling
 
 import "github.com/charmbracelet/lipgloss"
 
+var APViewport = lipgloss.NewStyle().
+	PaddingLeft(2)
+
 var APHelpBar = lipgloss.NewStyle().
 	Foreground(lipgloss.Color("#929693"))
