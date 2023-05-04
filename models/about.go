ef4cddb64b5859de0b625e891768fd46529518a7
Josh Weaver <jtw@digitalartbrokers.io>
2023-05-04 15:16:01 -0500

    Now reads file for the needed body of pager

--- models/about.go
+++ models/about.go
@@ -6,6 +6,7 @@ package models
 import (
 	"DAB-SSH/helpers"
 	"DAB-SSH/styling"
+	"os"
 	"strings"
 
 	"github.com/charmbracelet/bubbles/help"
@@ -24,6 +25,9 @@ type AboutPage struct {
 	minWidth  int              // Minimum Width so model won't break
 }
 
+// The about page DAB body
+var AboutContent, _ = os.ReadFile("content/about.md")
+
 /*
   ______ .______       _______     ___   .___________. _______     .___  ___.   ______    _______   _______  __
  /      ||   _  \     |   ____|   /   \  |           ||   ____|    |   \/   |  /  __  \  |       \ |   ____||  |
@@ -41,9 +45,9 @@ func CreateAboutPage() AboutPage {
 	NB := []string{"  Projects", "• About"}
 	WM := " DAB "
 
-	// Create Viewport
+	// Create Viewport and sets content
 	viewport := viewport.New(TerminalWidth, TerminalHeight-10)
-	viewport.SetContent(Content)
+	viewport.SetContent(string(AboutContent))
 
 	// Sets the help model and styling
 	help := help.New()
@@ -195,37 +199,3 @@ func (a AboutPage) View() string {
 	// Returns model with final styling
 	return styling.Border.Width(width).Height(height).Render(s)
 }
-
-// The about DAB body
-var Content string = `Mission:
-Digital Art Brokers (DAB) is a blockchain and NFT gamification company dedicated to creating
-unique gaming experiences while giving back through charitable partnerships.
-Buccaneers of the Blockchain (BotB) is DAB's first project, created in partnership with the national
-nonprofit organization DAV (Disabled American Veterans). BotB is a crypto-based game that allows
-players to learn the basics of blockchain gaming and engage in a transparent support system for
-disabled veterans – all while competing to earn real money.
-BotB is the first piece of a larger project aiming to illustrate DAB’s gamified model of service that
-re-imagines the donor/recipient relationship. DAB has plans to offer a host of experiences to offer
-another space where community-building and Web3 experience(s) can expand.
-DAB is committed to transparency, charitable impact, and providing unique gaming experiences.
-DAB’s ultimate objective is to utilize Web3, NFTs, and blockchain-based technologies to provide
-community value and initiate participation. Achievement of this will be satisfied by coding
-service into its tech, re-imagining “traditional” models of charitable giving, and
-re-conceptualizing the donor/recipient paradigm.
-BotB's partnership with DAV is an example of DAB's mission to do great while doing great. Players
-can enjoy a fun and competitive gaming experience while contributing to a worthy cause.
-
-Partnership with DAV:
-DAV empowers veterans to lead high-quality lives with respect and dignity. It is dedicated to a single
-purpose: keeping our promise to America's veterans. DAV ensures that veterans and their families
-can access the full range of benefits available to them, fights for the interests of America's injured
-heroes on Capitol Hill, provides employment resources to veterans and their families, and educates
-the public about the great sacrifices and needs of veterans transitioning back to civilian life.
-DAB has committed 25% (from mint) & 40% (of in-game purchases) of all BotB revenues to DAV, and
-any action performed within the game will result in further contribution. DAV CEO Mark Burgess is
-thrilled to partner with a great start-up company like DAB.
-
-DAB Head Developer Josh Ferguson has stated that bringing the DAV's purpose forward and into
-the blockchain space would demonstrate the far-reaching potential of the blockchain to support
-DAV in a wildly new and meaningful way – adding that he and the team anticipate great things to
-come.`
