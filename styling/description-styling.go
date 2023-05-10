58f36e2665e80072b5ea4e8c2e09e35370fdd427
Josh Weaver <jtw@digitalartbrokers.io>
2023-05-10 15:16:32 -0500

    Added viewport styling for description page

--- styling/description-styling.go
+++ styling/description-styling.go
@@ -2,5 +2,8 @@ package styling
 
 import "github.com/charmbracelet/lipgloss"
 
+var DPViewport = lipgloss.NewStyle().
+	PaddingLeft(2)
+
 var DPHelpBar = lipgloss.NewStyle().
 	Foreground(lipgloss.Color("#929693"))
